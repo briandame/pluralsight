e740714bbf5ed7fb5d1c340b30589e7c612032fa
brian <briandame@gmail.com>
2018-05-18 20:09:40 -0400

    Init

---
+++ src/test/java/com/briandame/pluralsight/PluralsightApplicationTests.java
@@ -0,0 +1,16 @@
+package com.briandame.pluralsight;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PluralsightApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
